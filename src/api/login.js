0ac2afa2f45df8a219c19873234e95949d531500
吕品超 <lvpinchao@xingheng.ai>
2025-12-11 11:09:13 +0800

    change url

--- src/api/login.js
+++ src/api/login.js
@@ -3,9 +3,9 @@ import request from './request';
 const url = 'https://note-express.vercel.app';
 
 export const goLogin = params => {
-  return request.post(`${url}/login`, params);
+  return request.post(`${url}/api/users/login`, params);
 };
 
 export const goRegistry = params => {
-  return request.post(`${url}/registry`, params);
+  return request.post(`${url}/api/users/registry`, params);
 };
